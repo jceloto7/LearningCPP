20e60ee79f9162795438dab7d9c4aea54a2f6f72
jceloto7 <jessicaceloto@gmail.com>
2022-07-14 19:37:20 -0300

    Add 'bom dia' folder

---
+++ bom dia/bom dia.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+# include <string>
+# include <sstream>
+using namespace std;
+
+int main (){
+string name;
+cout << "Qual é o seu nome? ";
+cin >> name;
+cout << "Bom dia " <<name +  " tudo bom?";
+
+system ("pause");
+
+
+}
+
+
+
+
+
+
+
